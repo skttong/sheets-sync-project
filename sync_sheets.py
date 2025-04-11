c056d8e6010c914d8c998318f5697c3fdb2c1611
skttong <sommanuttong@gmail.com>
2025-04-11 14:33:20 +0700

    commit

--- sync_sheets.py
+++ sync_sheets.py
@@ -3,14 +3,19 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 
+# โหลดจาก ENV
 service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+# กำหนด scope
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+
+# สร้าง credentials จากข้อมูล JSON
+creds = Credentials.from_service_account_info(service_account_info, scopes=scopes)
 client = gspread.authorize(creds)
 
-SHEET_A_ID = "1yg9oY2rmgAl7ogpkLAh0X3JYkryqa24_bOD86FXAhbU"
-SHEET_B_ID = "1k3jW7rc5U9rECJUuU8AJem2Q1sCZ1c3RxcGPLob-4Bo"
+# ใช้งาน Google Sheets
+SHEET_A_ID = "ใส่-ID-ชีตต้นทาง"
+SHEET_B_ID = "ใส่-ID-ชีตปลายทาง"
 
 sheet_a = client.open_by_key(SHEET_A_ID).sheet1
 sheet_b = client.open_by_key(SHEET_B_ID).sheet1
@@ -19,4 +24,4 @@ data_a = sheet_a.get_all_values()
 sheet_b.clear()
 sheet_b.update("A1", data_a)
 
-print("✅ ข้อมูลจาก Google Sheets A อัปเดตไป Google Sheets B สำเร็จ!")
+print("✅ อัปเดตข้อมูลเรียบร้อย")
