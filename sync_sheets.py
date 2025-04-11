b4d11df9305ea4b00c85f9fe5c2d283093cd9c36
skttong <sommanuttong@gmail.com>
2025-04-11 14:51:47 +0700

    comit

--- sync_sheets.py
+++ sync_sheets.py
@@ -14,8 +14,8 @@ creds = Credentials.from_service_account_info(service_account_info, scopes=scope
 client = gspread.authorize(creds)
 
 # ใช้งาน Google Sheets
-SHEET_A_ID = "1hzs1YLfr7kojHA6ZwVaGd2ygqqS7iCxB8jpFPXV7BZs"  # Google Sheets A (ต้นทาง)
-SHEET_B_ID = "1jYbOUjJw7SINbOb9MiMSFYhOu8CZ2iYIrXkHXTOqjng"  # Google Sheets B (ปลายทาง)
+SHEET_A_ID = "1yg9oY2rmgAl7ogpkLAh0X3JYkryqa24_bOD86FXAhbU"  # Google Sheets A (ต้นทาง)
+SHEET_B_ID = "1k3jW7rc5U9rECJUuU8AJem2Q1sCZ1c3RxcGPLob-4Bo"  # Google Sheets B (ปลายทาง)
 
 sheet_a = client.open_by_key(SHEET_A_ID).sheet1
 sheet_b = client.open_by_key(SHEET_B_ID).sheet1
